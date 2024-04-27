1edc40cf9d1e5ca40351c3bf999363054f8fb0b1
midohamdi1717 <abdelmaged.hamdi1@gmail.com>
2024-04-27 17:13:26 +0300

    upload

---
+++ project/src/SingleItem.jsx
@@ -0,0 +1,32 @@
+import { useDeleteTask, useEditTask } from "./reactQueryCustomHooks";
+
+const SingleItem = ({ item }) => {
+  const { deleteTask, isLoading } = useDeleteTask();
+  const { editTask } = useEditTask();
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.isDone}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.isDone })}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
+        }}
+      >
+        {item.title}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => deleteTask(item.id)}
+        disabled={isLoading}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
+export default SingleItem;
